d8cab5334e14797d1999ea3d24df2accbb8f2d2d
TomCatalina <42611260+noissues@users.noreply.github.com>
2023-04-08 22:45:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,53 +13,56 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江苏',
+  CITY: '南京',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o7pYV5wRKnUzmxHOxQxglw6q1VpI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '3MVlkxxoIMnX6T2A1fe0H2uRqOFVt-_jtWJCp46RaRI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-17',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日', name: '宝贝', year: '1998', date: '07-17',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-11-27' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '3MVlkxxoIMnX6T2A1fe0H2uRqOFVt-_jtWJCp46RaRI',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o7pYV5wRKnUzmxHOxQxglw6q1VpI',
     }
   ],
+  "SWITCH": {
+        "weather": true, 
+        "holidaytts": true, 
+        "CIBA": true, 
+        "oneTalk": false, 
+        "earthyLoveWords": false, 
+        "momentCopyrighting": false, 
+        "poisonChickenSoup": false, 
+        "poetry": false, 
+        "horoscope": true, 
+        "birthdayMessage": true
+    }
 
 }
 
