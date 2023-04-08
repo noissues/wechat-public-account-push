823471ca5cf39a4dfd85a280dfa259916e763042
TomCatalina <42611260+noissues@users.noreply.github.com>
2023-04-08 23:00:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7pYV5wRKnUzmxHOxQxglw6q1VpI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3MVlkxxoIMnX6T2A1fe0H2uRqOFVt-_jtWJCp46RaRI',
+      useTemplateId: 'O_k6ob1PWzTuejMnHPYq6sV2wkYApLkoB0aDtAkRfsg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-17',
       festivals: [
