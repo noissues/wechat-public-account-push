b7dfe61f7737893a89290210e6cf8e00213762fe
花载酒 <42611260+noissues@users.noreply.github.com>
2023-12-19 20:45:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o7pYV5_4A204o1nriaLkeceYh5O4',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '913kD-auQBOm4chAeY_f6kM7QLULCrIFTRxBAa9ATFA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -45,7 +45,7 @@ const USER_CONFIG = {
       id: 'o7pYV5wRKnUzmxHOxQxglw6q1VpI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // useTemplateId: 'O_k6ob1PWzTuejMnHPYq6sV2wkYApLkoB0aDtAkRfsg',
-      useTemplateId: '-XJ1FFNCIQpOu0CPwWE_E3YBFPSB1HNXEmMamtsY0Ek',
+      useTemplateId: '913kD-auQBOm4chAeY_f6kM7QLULCrIFTRxBAa9ATFA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-17',
       festivals: [
